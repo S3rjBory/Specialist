14fb07b8e3a5dac33b3b1ed5c80b204c873ca739
S3rjBory <45003303+S3rjBory@users.noreply.github.com>
2018-11-13 17:29:57 +0300

    Update T_bot.py

--- T_bot.py
+++ T_bot.py
@@ -4,36 +4,59 @@ import datetime
 import telepot
 from telepot.loop import MessageLoop
 import requests, json
-
 import urllib3
-
 from pathlib import Path
+import os
 
-proxy_url = "http://94.243.140.162:40960" # http://proxy_server:port
+proxy_url = "http://94.243.140.162:40960"
 telepot.api._pools = {
     'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=80),
 }
 telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
 
-
-
 print(strftime('%H:%M:%S'))
 
-
 def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    print(content_type, chat_type, chat_id)
+    
     chat_id=msg['chat']['id']
-    command=msg['text']
+    #command=msg['text']
+
+    content_type, chat_type, chat_id = telepot.glance(msg,'chat')
+    chat_idint=str(chat_id)
+    print(chat_idint)
 
     print('Recieved: ')
-    print(command)
+    #print(command)
 
-    if command == 'hi':
-        bot.sendMessage(chat_id, str('Hi'))
+    if content_type == 'text':
+        command=msg['text']
+        if command == 'hi':
+            bot.sendMessage(chat_id, str('Hi'))
+        if command == 'id':
+            file_id = msg['chat']['id']
+            print(chat_id)
+            bot.sendMessage(chat_id, str(file_id))
+       #bot.sendDocument(chat_id, open("/ICON.ICO", 'rb'))
+    
+    if content_type == 'photo':
         
+        try:
+            os.mkdir('C:/tmp/'+chat_idint, mode=0o777)
+        except OSError as error:
+            print(error)
+       
+        try:
+            bot.download_file(msg['photo'][-1]['file_id'], 'C:/tmp/'+chat_idint+'/'+msg['photo'][-1]['file_id']+'.png')
+            bot.sendMessage(char_id, str('Done')
+            bot.sendMessage(chat_id, str(msg['photo'][-3]['file_id']))  
+        except Exception as e:
+            bot.sendMessage(chat_id, str('No'))
+            print(e)
 
-bot=telepot.Bot('522725344:AAGyVp0GjWPzYvW_BKaL3qBEj-ibOiXxwWY') # @RaspberrySerjTestBot
+bot=telepot.Bot('522725344:AAGyVp0GjWPzYvW_BKaL3qBEj-ibOiXxwWY') #@RaspberrySerjTestBot
 print(bot.getMe())
-
 
 MessageLoop(bot, handle).run_as_thread()
 print("Listening...")
@@ -44,3 +67,4 @@ try:
         print('Time is: '+strftime('%H:%M:%S'))
 except KeyboardInterrupt:
     pass
+
